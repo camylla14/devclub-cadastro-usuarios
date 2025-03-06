d246450282fa2df589d7f431de6609e32e236d6c
camylla14 <vcamylla222@gmail.com>
2025-03-06 17:32:57 -0300

    inicia a criação da tela de cadastro de usuários

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
-import { Title, Container } from './styles'
+import { Title, Container, TopBackground, Form, ContainerInputs, Input, Button, InputLabel } from './styles'
 
-function App() {
+function Home() {
 
   return (
     <Container>
-      <Title>Ok React</Title>
+      <TopBackground>
+        <img />
+      </TopBackground>
+
+
+      <Form>
+        <Title>Cadastrar Usuários</Title>
+
+        <ContainerInputs>
+          <div>
+            <div>
+              <InputLabel>
+                Nome<span>*</span>
+              </InputLabel>
+              <Input type='text' placeholder='Nome do Usuário' />
+            </div>
+
+            <div>
+              <InputLabel>
+                Idade<span>*</span>
+              </InputLabel>
+              <Input type='number' placeholder='Idade do usuário' />
+            </div>
+          </div>
+
+          <div>
+            <InputLabel>
+              E-mail<span>*</span>
+            </InputLabel>
+            <Input type='email' placeholder='E-mail do usuário' />
+          </div>
+        </ContainerInputs>
+
+        <Button>Cadastrar Usuários</Button>
+      </Form>
     </Container>
   )
 }
 
-export default App
+export default Home
 
 /*
 dois jeitos de exportar
