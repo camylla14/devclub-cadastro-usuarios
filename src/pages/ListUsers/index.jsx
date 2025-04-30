52e25e0d44431ec915c5146a1f188c6d0b561821
camylla14 <vcamylla222@gmail.com>
2025-04-30 20:55:45 -0300

    estiliza a segunda tela (tela de cadastro)

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -3,36 +3,61 @@ import api from '../../services/api'
 
 import Button from '../../components/Button'
 import TopBackground from '../../components/TopBackground'
+import Trash from '../../assets/trash.svg'
+
+
+import { 
+     Container,
+     ContainerUsers,
+     CardUsers,
+     TrashIcon,
+     Title,
+     AvatarUser,
+    } from './styles'
 
 
 function ListUsers() {
-const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([])
 
     useEffect(() => {
         async function getUsers() {
             const { data } = await api.get('/usuarios')
-            
+
             setUsers(data)
         }
         getUsers()
     }, [])
 
+async function deleteUsers(id){
+   await api.delete(`/usuarios/${id}`)
+   
+   const upadatedUsers = users.filter( user => user.id !== id )
+
+   setUsers(upadatedUsers)
+}
 
     return (
-        <div>
+        <Container>
             <TopBackground />
+            <Title>Lista de Usuários</Title>
 
-            <h1>Listagem de Usuários</h1>
 
-            {users.map((user) => (
-                <div key={user.id}>
-                    <p>{user.name}</p>
-                    <p>{user.email}</p>
-                    <p>{user.age}</p>
-                </div>
-            ))}
-            <Button>Voltar</Button>
-        </div>
+            <ContainerUsers>
+                {users.map((user) => (
+                    <CardUsers key={user.id}>
+                        <AvatarUser src={`https://avatar.iran.liara.run/public?username=${user.id}`}/>
+                        <div>
+                            <h3>{user.name}</h3>
+                            <p>{user.email}</p>
+                            <p>{user.age}</p>
+                        </div>
+                        <TrashIcon src={Trash} alt='icone-lixo' onClick={() => deleteUsers(user.id)}/>
+                    </CardUsers>
+                ))}
+            </ContainerUsers>
+
+            <Button type="button" >Voltar</Button>
+        </Container>
     )
 }
 
